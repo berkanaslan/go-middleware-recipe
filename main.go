f079239dc67c6c98d4bb2dd89818aa4ebbd94975
Berkan Aslan <aslnberkan@gmail.com>
2023-09-04 18:32:45 +0300

    Project initialized.

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	err := app.Listen(":3000")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
