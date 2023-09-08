8cc45bf13162b6b9948f703df3cf3b41d20a3436
Berkan Aslan <aslnberkan@gmail.com>
2023-09-08 03:39:40 +0300

    I added repository layer for crud operations.
    TODO: Instead using T any for generic, use T core.BaseEntity later on.

---
+++ model/core/user.go
@@ -0,0 +1,38 @@
+package core
+
+import "golang.org/x/crypto/bcrypt"
+
+type User struct {
+	ID uint `json:"id" gorm:"primary_key"`
+	Audit
+	Email    string `json:"email" gorm:"type:varchar(255);unique;not null"`
+	Password `json:"password" gorm:"type:varchar(255);not null"`
+	UserRole UserRole `json:"user_role" gorm:"type:varchar(255);not null;default:'user'"`
+	Enabled  bool     `json:"enabled" gorm:"index;default:true"`
+}
+
+func (u *User) GetID() uint {
+	return u.ID
+}
+
+func (u *User) TableName() string {
+	return "user"
+}
+
+func (u *User) SetPassword(password string) error {
+	hashedPassword, err := HashPassword(password)
+
+	if err != nil {
+		return err
+	}
+
+	pointerToUser := &u
+	(*pointerToUser).Password = hashedPassword
+
+	return nil
+}
+
+func (u *User) CheckPasswordHash(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
---
+++ repository/base_entity_repository.go
@@ -0,0 +1,42 @@
+package repository
+
+import (
+	"fmt"
+	"go-middleware-recipe/database"
+)
+
+type Repository[T any] interface {
+	Create(data T) (T, error)
+	Read(id int) (T, error)
+	Update(data T) error
+	Delete(id int) error
+}
+
+type Impl[T any] struct {
+}
+
+func (r *Impl[T]) Create(data T) (T, error) {
+	tx := database.DBConn.Create(&data)
+
+	if tx.Error != nil {
+		return data, tx.Error
+	}
+
+	return data, nil
+}
+
+func (r *Impl[T]) Read(id int) (T, error) {
+	var entity T
+	tx := database.DBConn.First(&entity, id)
+	return entity, tx.Error
+}
+
+func (r *Impl[T]) Update(data T) error {
+	fmt.Println("RepositoryImpl Update")
+	return nil
+}
+
+func (r *Impl[T]) Delete(id int) error {
+	fmt.Println("RepositoryImpl Update")
+	return nil
+}
